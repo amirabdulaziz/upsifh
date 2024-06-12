a96c80a99fdf28668a6648dc4e054955a070b2e0
amirdotcom <amirabdulaziz01@gmail.com>
2024-06-12 23:06:19 +0800

    home.tsx page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+import FitnesHome from "../assets/homeimg/fitneshome.svg";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
+      <IonHeader className="ion-no-border">
+        <IonToolbar>{/* Toolbar content (if any) */}</IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div className="flex flex-col justify-center items-center h-full relative">
+          <div className="flex flex-col justify-center items-center">
+            <IonImg src={FitnesHome} className="mb-4 h-[240px] w-[240px]" />
+            <IonText className="text-black text-center font-bold space-y-1 py-2">
+              <h2 className="text-2xl">Welcome to UPSIFH</h2>
+              <p className="font-semibold text-sm">Your fitness Helper</p>
+            </IonText>
+          </div>
+          <div className="w-full absolute bottom-4 px-4">
+            <button
+              className="bg-[#D4C685] rounded-lg shadow-md px-2 py-2 h-auto w-full"
+            >
+              <IonText className="text-white">
+                <p>Get started now</p>
+              </IonText>
+            </button>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#b236ff",
+        // primary: "#DEBCFF",
+        secondary: "#000000",
+        tertiary: "#FFFFFF",
+        fav: "#b236ff",
+      },
+      text: {
+        primary: "#b263ff",
+        secondary: "#000000",
+        tertiary: "#FFFFFF",
+        fav: "#b236ff",
+      }
+    },
   },
   plugins: [],
 };
